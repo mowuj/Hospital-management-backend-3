591db090d904577a54dd17015edebbf982b3ec08
mowuj <mowuj92@gmail.com>
2024-01-16 22:28:07 +0600

    registration

--- patient/views.py
+++ patient/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .import models
 from . import serializers
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 class PatientViewset(viewsets.ModelViewSet):
     queryset=models.Patient.objects.all()
     serializer_class=serializers.PatientSerializer
+
+class UserRegistrationApiView(APIView):
+    serializer_class=serializers.RegistrationSerializer
+
+    def post(self,request):
+        serializer=self.serializer_class(data=request.data)
+        
+        if serializer.is_valid():
+            user=serializer.save()
+            print(user)
+            return Response("Done")
+        return Response(serializer.errors)
